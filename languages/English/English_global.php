c0f515af81b4633be3646f3659f70f1abcb6c90c
Gábor <yesszus@gmail.com>
2016-10-31 20:36:30 +0100

    Add files via upload

---
+++ languages/English/English_global.php
@@ -0,0 +1,19 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/10/31 19:18:43
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_SUMMARY", "Embed YouTube channels and playlists into e107.");
+define("LAN_PLUGIN_DESCRIPTION", "Use YouTube RSS or API feeds to generate video pages for your e107 website.");
+define("LAN_YOUTUBE_SUBSCRIBE_CHANNEL", "Subscribe to this Channel");
+define("LAN_YOUTUBE_NONE_AVAILABLE", "None available!");
+define("LAN_YOUTUBE_BAD_LINK", "Bad Link!");
+
+?>
