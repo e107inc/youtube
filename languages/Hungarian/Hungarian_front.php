e7bb5bd3ec71e26ddcebd7b9746d021d0a734a01
Gábor <yesszus@gmail.com>
2016-11-01 09:21:50 +0100

    Some corrections

--- languages/Hungarian/Hungarian_front.php
+++ languages/Hungarian/Hungarian_front.php
@@ -10,7 +10,9 @@
 +---------------------------------------------------------------+
 */
 
-define("LAN_PLUGIN_SUMMARY", "YouTube csatorna és lejátszási lista beágyazása e107 weboldalakra.");
-define("LAN_PLUGIN_DESCRIPTION", "Használja a YouTube RSS vagy API-hírcsatornákat videók generálásához az e107 honlapjára.");
+define("LAN_YOUTUBE_SUBSCRIBE_CHANNEL", "Feliratkozás a csatornára");
+define("LAN_YOUTUBE_NONE_AVAILABLE", "Nem elérhető!");
+define("LAN_YOUTUBE_BAD_LINK", "Hibás Link!");
+define("LAN_YOUTUBE_VIDEOS", "Videók");
 
 ?>
