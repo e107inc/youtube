935f8fc15fc6d98a6f4d4bed168c8c2941def5fe
veskoto <v.totev@corllete.com>
2017-06-20 14:01:55 +0300

    Bulgarian language for YouTube e107 plugin

---
+++ languages/Bulgarian/Bulgarian_front.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
+|        $Author: veskoto $
++---------------------------------------------------------------+
+*/
+
+define("LAN_YOUTUBE_001", "Абониране за канала");
+define("LAN_YOUTUBE_002", "Няма нищо!");
+define("LAN_YOUTUBE_003", "Кофти линк!");
+define("LAN_YOUTUBE_004", "Видео");
+
+?>
---
+++ languages/Bulgarian/Bulgarian_global.php
@@ -0,0 +1,15 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
+|        $Author: veskoto $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_SUMMARY", "Вмъкване на канали и плейлисти от YouTube в e107.");
+define("LAN_PLUGIN_DESCRIPTION", "Използвайте RSS емисии или приложни програмни интерфейси (API) от YouTube, за да генерирате видео страници в е107 уебсайта си.");
+
+?>
